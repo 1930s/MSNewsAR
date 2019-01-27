800ea836f35eceb7d26d94dfac074f1bfee3b8f8
Superzzp <zhangzhipian1@gmail.com>
2019-01-26 18:09:53 -0800

    add video playback on image

--- NewsAR/ViewController.swift
+++ NewsAR/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -23,19 +23,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsImages", bundle: Bundle.main) {
+            
+            configuration.trackingImages = trackedImages
+            
+            configuration.maximumNumberOfTrackedImages = 2
+            
+        }
+        
+        
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +53,99 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    
+    func setReferenceImgFromURL(){
+        
+    }
+    
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            
+            //            if (imageAnchor.referenceImage.name == "salad") {
+            //                print("salad!!!!!!")
+            //                let videoNodeTest = SKVideoNode(fileNamed: "test.mp4")
+            //
+            //                videoNodeTest.play()
+            //
+            //                let videoTestScene = SKScene(size: CGSize(width: 480, height: 360))
+            //
+            //                videoNodeTest.position = CGPoint(x: videoTestScene.size.width / 2, y: videoTestScene.size.height / 2)
+            //
+            //                videoNodeTest.yScale = -1.0
+            //
+            //                videoTestScene.addChild(videoNodeTest)
+            //
+            //
+            //                testMaterial.diffuse.contents = videoTestScene
+            //            }
+            
+            
+            if (imageAnchor.referenceImage.name == "harrypotter") {
+                print("pottererrrr!!!")
+                
+                let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+                videoNode.play()
+                let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+                videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+                videoNode.yScale = -1.0
+                videoScene.addChild(videoNode)
+                
+                let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+                let hpMaterial: SCNMaterial = SCNMaterial()
+                hpMaterial.diffuse.contents = videoScene
+                plane.firstMaterial?.diffuse.contents = videoScene
+                let planeNode = SCNNode(geometry: plane)
+                planeNode.eulerAngles.x = -.pi / 2
+                node.addChildNode(planeNode)
+                
+            }
+            
+            
+            if (imageAnchor.referenceImage.name == "salad") {
+                print("saladdddd!!!")
+                
+                let videoNode = SKVideoNode(fileNamed: "test.mp4")
+                let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+                videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+                videoNode.yScale = -1.0
+                videoScene.addChild(videoNode)
+                
+                let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+                let hpMaterial: SCNMaterial = SCNMaterial()
+                hpMaterial.diffuse.contents = videoScene
+                plane.firstMaterial?.diffuse.contents = videoScene
+                let planeNode = SCNNode(geometry: plane)
+                planeNode.eulerAngles.x = -.pi / 2
+                node.addChildNode(planeNode)
+                
+            }
+            
+            
+            
+            
+            //==========second video node
+            
+            
+            //add all materials to plane
+            //plane.firstMaterial? = hpMaterial
+            
+            //plane.materials.append(hpMaterial)
+            
+            
+            
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
